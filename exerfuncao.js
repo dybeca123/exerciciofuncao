a4d5741ca6444dc1218c0b21166b06e803869d4b
dybeca123 <103453892+dybeca123@users.noreply.github.com>
2022-05-05 15:04:59 -0300

    Add files via upload

---
+++ exerfuncao.js
@@ -0,0 +1,35 @@
+//At 01-a
+
+function nome (n){ 
+console.log (n);
+return
+}
+
+nome ('Rebeca');
+
+//At 2
+
+function soma (d,e){   
+console.log (d + e)
+return
+}
+
+soma (4, 2)
+
+//At 3
+
+function multiplicacao (f,g){
+    console.log (f*g)
+    return
+}
+
+multiplicacao (2,5)
+
+//At 4
+
+function media (a, b, c, d){
+    console.log ((a + b + c + d)/4)
+    return
+}
+
+media (4,4,2,2)
